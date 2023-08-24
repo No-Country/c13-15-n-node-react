5f2ac568006b43e8cf30d8460c920eeb2a186c82
Gustavo Cubilla <cubilladev@gmail.com>
2023-08-24 19:32:37 -0400

    Creacion de routers y la rama de front

--- Client/eCalendary/src/main.jsx
+++ Client/eCalendary/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+
+import router from './routers/router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <NextUIProvider>
-        <App />
+        <RouterProvider router={router} />
       </NextUIProvider>
   </React.StrictMode>,
 )
