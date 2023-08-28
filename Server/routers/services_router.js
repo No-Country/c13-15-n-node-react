b58135eb341258708d5d0d91249fadc2041d2476
Gustavo Cubilla <cubilladev@gmail.com>
2023-08-28 09:42:13 -0400

    Reparacion de conflicto de router

--- Server/routers/services_router.js
+++ Server/routers/services_router.js
@@ -1,6 +1,6 @@
-const express     = require('express');
-const router      = express.Router();
-const { create_service }   = require('../controllers/services_controller')
+const express = require('express');
+const router = express.Router();
+const { create_service } = require('../controllers/services_controller')
 
 router.post('/', create_service );
 
