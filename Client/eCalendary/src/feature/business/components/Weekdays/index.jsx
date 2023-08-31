f8d623576d905e402d60d24505220b0eb767fd8c
Daniel Barraza <daniel.ba74@gmail.com>
2023-08-31 18:45:16 -0300

    formularios input

--- Client/eCalendary/src/feature/business/components/Weekdays/index.jsx
+++ Client/eCalendary/src/feature/business/components/Weekdays/index.jsx
@@ -1,18 +1,6 @@
 import {Select, SelectItem, SelectSection} from "@nextui-org/react";
 import style from './style.module.css'
 
-//Dias de la semana
-
-const dias = [
-  { key: "00", text: "Domingo"},
-  { key: "01", text: "Lunes"},
-  { key: "02", text: "Martes"},
-  { key: "03", text: "Miercoles"},
-  { key: "04", text: "Jueves"},
-  { key: "05", text: "Viernes"},
-  { key: "06", text: "Sabado"},
-]
-
 export default function Days() {
   return (
     <Select
--- Client/eCalendary/src/feature/business/pages/business.jsx
+++ Client/eCalendary/src/feature/business/pages/business.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { Button } from '@nextui-org/react'
 import { Textarea1 } from '../components'
 import MySelect from '../components/MySelect'
+import { Hora, dias, intervalo, meses } from '../../../_mocks_/bussinessMocks'
 
 const Business = () => {
   return (
     <div className=' '>
       Business
-      <Textarea1></Textarea1>
-      <MySelect></MySelect>
+      <Textarea1 />
+      <MySelect lista={meses} titulo="Meses de Año" seleccionMultiple = {true}/>
+      <MySelect lista={dias} titulo="Dias" seleccionMultiple = {true}/>
+      <MySelect lista={Hora} titulo="Hora" seleccionMultiple = {true}/>
+      <MySelect lista={intervalo} titulo="Intervalo" />
+      
       <Button color="primary">
         Crear
       </Button>
