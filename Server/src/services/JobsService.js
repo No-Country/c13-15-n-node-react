196ffa84acfc4e7bf365cdbe1e0b3524bca4740a
emanuelgauler <emanuelgauler@gmail.com>
2023-09-06 22:12:21 -0300

    se agregó soporte para la generación de días según los solicitado por el cliente

--- Server/src/services/JobsService.js
+++ Server/src/services/JobsService.js
@@ -1,7 +1,9 @@
-const { v4: uuidv4 } = require('uuid');
-const JobModel = require('../models/servicioModel');
-const JobScheduled = require('../models/horariosServiciosModel')
-const UserModel   = require('../models/usuarioModel')
+const { v4: uuidv4 }    = require('uuid');
+const day_generator     = require('../utils/days_generator');
+const JobModel          = require('../models/servicioModel');
+const JobScheduled      = require('../models/horariosServiciosModel')
+const UserModel         = require('../models/usuarioModel')
+const ScheduleModel     = require('../models/horarioModel');
 
 const JobsService = class {
    async create(job_data) {
@@ -15,10 +17,24 @@ const JobsService = class {
             , duration: job_data.duration
          });
 
+         console.log( job_data.months, job_data.days );
+
+         console.log( day_generator( {
+            "months": job_data.months,
+            "days": job_data.days
+         }) );/*.array.forEach( async element => {
+            await ScheduleModel.create({
+               schedule_id: uuidv4(), schedule: element
+            })
+         });
+         */
+
          return job;
       } catch (error) {
          if( error.name === 'SequelizeUniqueConstraintError' ) {
             throw { code: 1, message: `${job_data.name} ya existe` }
+         } else {
+            throw { code: -1, message: `DESCONOCIDO - ${error}` }
          }
       }
    }
