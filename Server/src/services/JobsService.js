c770148c121de1a58e91c90370c735c5c30866b6
emanuelgauler <emanuelgauler@gmail.com>
2023-09-07 17:34:23 -0300

    crea el servicio y asocia los dias

--- Server/src/services/JobsService.js
+++ Server/src/services/JobsService.js
@@ -1,9 +1,9 @@
-const { v4: uuidv4 }    = require('uuid');
-const day_generator     = require('../utils/days_generator');
-const JobModel          = require('../models/servicioModel');
-const JobScheduled      = require('../models/horariosServiciosModel')
-const UserModel         = require('../models/usuarioModel')
-const ScheduleModel     = require('../models/horarioModel');
+const { v4: uuidv4 }       = require('uuid');
+const generate_days_from   = require('../utils/days_generator');
+const JobModel             = require('../models/servicioModel');
+const JobScheduled         = require('../models/horariosServicioModel')
+const UserModel            = require('../models/usuarioModel')
+const ScheduleModel        = require('../models/horarioModel');
 
 const JobsService = class {
    async create(job_data) {
@@ -17,22 +17,24 @@ const JobsService = class {
             , duration: job_data.duration
          });
 
-         console.log( job_data.months, job_data.days );
-
-         console.log( day_generator( {
+         const days_schedules = generate_days_from( {
             "months": job_data.months,
             "days": job_data.days
-         }) );/*.array.forEach( async element => {
-            await ScheduleModel.create({
+         }).forEach( async element => {
+            const schedule = await ScheduleModel.create({
                schedule_id: uuidv4(), schedule: element
             })
+            job.addSchedule( schedule, { though: { available: false } } );
+            console.log( ">> Fecha programada: ", schedule.schedule );
          });
-         */
+         /**/
 
          return job;
       } catch (error) {
          if( error.name === 'SequelizeUniqueConstraintError' ) {
             throw { code: 1, message: `${job_data.name} ya existe` }
+         } else if( error.name === 'SequelizeValidationError') {
+            throw { code: 2, message: `Error al validar los datos: ${error}`}
          } else {
             throw { code: -1, message: `DESCONOCIDO - ${error}` }
          }
