c9c9ba6af60ae670abcea8c60a9babf8b166ae9c
diazarm <marceloardiaz@gmail.com>
2023-09-15 01:00:50 -0300

    update

--- Client/eCalendary/src/feature/home/component/hero.jsx
+++ Client/eCalendary/src/feature/home/component/hero.jsx
@@ -9,12 +9,10 @@ const Hero = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Agenda Digital Los Tres Primeros Meses Gratis{/* Free Landing Page Template for startups */}
+            Obtén tres meses gratis de nuestra Agenda Digital
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              E-Calendary es una Agenda Digital que presta 
-              Servicio a los Profecionales y sus Clientes. 
-              Estamos para facilitar el trabajo los 365 días del Año.
+            E-Calendary es una Agenda Digital que brinda servicios tanto a profesionales como a sus clientes. Estamos comprometidos en facilitar su trabajo durante los 365 días del año.
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
